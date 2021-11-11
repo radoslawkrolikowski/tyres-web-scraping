1282e9c07eafe8fede30c7a707aeb6483736bd26
unknown <radoslawkrolikowski4@gmail.com>
2021-11-11 12:57:21 +0000

    Set the pipeline responsible for saving data to CSV file

--- tyres/tyres/pipelines.py
+++ tyres/tyres/pipelines.py
@@ -6,8 +6,24 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+from scrapy.exporters import CsvItemExporter
 
 
-class TyresPipeline:
+class TyresToCsvPipeline:
+    def __init__(self, filepath):
+        self.filepath = filepath
+        self.file = open(self.filepath, 'w+b')
+        self.exporter = CsvItemExporter(self.file)
+        self.exporter.start_exporting()
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls(filepath=crawler.spider.filepath)
+
     def process_item(self, item, spider):
+        self.exporter.export_item(item)
         return item
+
+    def close_spider(self, spider):
+        self.exporter.finish_exporting()
+        self.file.close()
