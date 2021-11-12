f36df303c73ae1d686bb6258d1c284ae669ecc6c
unknown <radoslawkrolikowski4@gmail.com>
2021-11-12 14:25:08 +0000

    Correct the returned tyre_size value

--- tyres/tyres/spiders/dexelSpider.py
+++ tyres/tyres/spiders/dexelSpider.py
@@ -111,7 +111,7 @@ class DexelspiderSpider(scrapy.Spider):
                 'profile': self.profile,
                 'rim': self.rim,
                 'tyre_pattern': tyre_pattern,
-                'tyre_size': " ".join(description),
+                'tyre_size': tyre_size,
                 'price': price,
                 'fuel': fuel,
                 'wetgrip': wetgrip,
